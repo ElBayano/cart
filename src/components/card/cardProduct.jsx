93a00f26833e81753789fdf1e97b0b26cd20aaee
ElBayano <levitadejesus15@gmail.com>
2023-07-31 20:35:51 -0300

    feat: criação do cardProduct

---
+++ src/components/card/cardProduct.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {CardProduct} from './cardProduct.style'
+function CardProduct(){
+    return(
+        <CardProduct/>
+    )
+}
+
+export default CardProduct
---
+++ src/components/card/cardProduct.style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const CardProduct = styled.div`
+    height: 15rem;
+    background-color: red;
+`

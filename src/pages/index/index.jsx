0ac3b286653d04c3e03058204931ee176a56d741
Samuel Paixão Nascimento <samdeveloperbr@gmail.com>
2023-08-01 23:51:44 +0000

    feat: edição da pagina index e adição de um seletor de limite de produtos para mostrar

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -5,19 +5,34 @@ import { useEffect, useState } from "react";
 function IndexPage() {
     
     const [product, setProduct] = useState([])
-
-    const DownloadProduct = () => { api.get('products?limit=10').then(res => setProduct(res.data)) }
+    const [ productLimit, setProdutctLimit ] = useState(4)
+    const DownloadProduct = () => { api.get(`products?limit=${productLimit}`).then(res => setProduct(res.data)) }
+    const newLimit = (event) => {setProdutctLimit(event.target.value)}
 
     useEffect(() => {
         DownloadProduct()
-        console.log(product)
     }, [])
 
-    return(<>
-    {/* <CardProduct name={product.title} price={product.price} img={product.image}/> */}
-        {product.map(item => <CardProduct key={item.id} name={item.title} price={item.price} img={item.image} /> )}
-        </>
-)
+    useEffect(() => {
+        DownloadProduct()
+    }, [productLimit])
+
+    
+
+    return(
+        <>
+            <select onChange={newLimit} >
+                <option value="4" >4</option>
+                <option value="6" >6</option>
+                <option value="10" >10</option>
+                <option value="20" >20</option>
+            </select>
+            {product.map(item => <CardProduct 
+                        key={item.id}
+                        name={item.title}
+                        price={item.price}
+                        img={item.image} />)}
+        </>)
 }
 
 export default IndexPage
